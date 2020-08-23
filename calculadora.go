a9c3cc1f50ee4b6f68206afde1f2bec8f81b5d04
Fquilar <quilarque1@gmail.com>
2020-08-23 00:30:00 -0400

    cambiando leerEntrada

--- calculadora.go
+++ calculadora.go
@@ -8,9 +8,9 @@ import (
 	"strings"
 )
 
-type calc struct {}
+type Calc struct {}
 
-func (calc) operate (entrada string, operador string) int {
+func (Calc) Operate (entrada string, operador string) int {
 	entradaLimpia := strings.Split(entrada, operador)
 	valor1 := parsearOperador(entradaLimpia[0])
 	valor2 := parsearOperador(entradaLimpia[1])
